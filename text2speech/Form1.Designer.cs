441ddf37b854518408fcbb8b5813003c5a2a4ae3
a <tohellhi@gmail.com>
2021-05-30 01:14:10 +0330

    Add project files.

---
+++ text2speech/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace texttospeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "play/resume";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 1);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(589, 395);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 10);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 396);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(589, 47);
+            this.panel1.TabIndex = 5;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(389, 10);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(139, 27);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1Scroll);
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TrackBar1MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 10);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 27);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "slower";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 10);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 27);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "faster";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(589, 443);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Practice correct pronunciation of English words";
+            this.Load += new System.EventHandler(this.Form1Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ text2speech/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Threading;
+
+namespace texttospeech
+{
+    public partial class Form1 : Form
+    {
+        int _speed;
+        SpeechSynthesizer _speaker;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                if (_speaker.State==SynthesizerState.Paused)
+                {
+                    _speaker.Resume();
+                    return;
+                }
+                if (!(_speaker.State == SynthesizerState.Speaking))
+                {
+                    _speaker = new SpeechSynthesizer
+                                   {
+                                       Volume = trackBar1.Value,
+                                       Rate = _speed
+                                   };
+
+                    // speaker.Speak(textBox1.Text);
+                    _speaker.SpeakCompleted += speaker_SpeakCompleted;
+                    //speaker.SelectVoice("a");
+                    //speaker.SetOutputToWaveFile("d:\\a.wav");
+
+                    _speaker.SpeakAsync(textBox1.Text);
+                }
+            }
+            catch
+            {
+                _speaker.Resume();
+            }
+        }
+
+        void speaker_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            //speaker=null;
+        }
+
+        private void Button2Click(object sender, EventArgs e)
+        {
+              _speaker.Pause();
+        }
+
+        private void Button3Click(object sender, EventArgs e)
+        {
+            if (_speed >= 10)
+            { 
+                button3.Enabled = false;
+                _speed = 10; return; 
+            }
+            button4.Enabled = true;
+            _speed+=5;
+            
+            _speaker.Rate = _speed;
+
+            if (_speaker != null)
+            {
+                _speaker.Pause();
+                _speaker.Resume();
+            }
+        }
+
+        private void Button4Click(object sender, EventArgs e)
+        {
+            if (_speed <= -10)
+            {
+                button4.Enabled = false;
+                _speed = -10; return;
+            }
+            button3.Enabled = true;
+            _speed -= 5;
+
+
+            _speaker.Rate = _speed;
+            if (_speaker != null)
+            {
+                _speaker.Pause();
+                _speaker.Resume();
+            }
+        }
+
+        private void TrackBar1Scroll(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TrackBar1MouseUp(object sender, MouseEventArgs e)
+        {
+            _speaker.Volume = trackBar1.Value;
+        }
+
+        private void Form1Load(object sender, EventArgs e)
+        {
+            _speaker = new SpeechSynthesizer();
+        }
+
+   
+
+     }
+}
